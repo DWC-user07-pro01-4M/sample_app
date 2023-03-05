124451f8432516c5c1ca47f5652dfbd3d6fedf50
DWC-user07- <124852092+DWC-user07->
2023-03-05 07:47:55 +0000

    [Finish]一覧画面を表示しよう

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
+  get 'top' => 'homes#top'
 
   get 'lists/new'
 
   post 'lists' => 'lists#create'
 
-  get 'lists/index'
+  get 'lists' => 'lists#index'
 
   get 'lists/show'
 
